c6cfa35b37b7dafe51c0160777ca476b19df684b
necro-wbj <d04220837@gm.tut.edu.tw>
2018-07-29 23:03:38 +0800

    backup

--- ReadFile.py
+++ ReadFile.py
@@ -1,24 +1,24 @@
 #!python3
 import csv
 
-#DictReader
-f=open('test3.csv','r')
+# DictReader
+f = open('test3.csv', 'r')
 reader = csv.DictReader(f)
 
 # next(reader)
 for line in reader:
-	print(line['A'])
-	print(line['B'])
-	print(line['C'])
-	print('-------')
+    print(line['A'])
+    print(line['B'])
+    print(line['C'])
+    print('-------')
 
-#Reader
-f=open('test3.csv','r')
+# Reader
+f = open('test3.csv', 'r')
 reader = csv.reader(f)
 
-next(reader)#捨棄表頭
+next(reader)  # 捨棄表頭
 for line in reader:
-	print(line[0])
-	print(line[1])
-	print(line[2])
-	print("-------")
+    print(line[0])
+    print(line[1])
+    print(line[2])
+    print("-------")
